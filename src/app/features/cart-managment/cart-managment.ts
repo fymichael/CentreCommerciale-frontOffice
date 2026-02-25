bf6fb95058cede33a11bd2362a41959532818419
fymichael <118510462+fymichael@users.noreply.github.com>
2026-02-25 22:59:34 +0300

    modify cart managment interface

--- src/app/features/cart-managment/cart-managment.ts
+++ src/app/features/cart-managment/cart-managment.ts
@@ -3,13 +3,21 @@ import { Header } from '../../shared/header/header';
 import { Footer } from '../../shared/footer/footer';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart-managment',
-  imports: [Header, Footer, MatIconModule, RouterModule],
+  imports: [Header, Footer, MatIconModule, RouterModule, CommonModule, FormsModule],
   templateUrl: './cart-managment.html',
   styleUrl: './cart-managment.scss',
 })
 export class CartManagment {
+  public item1Selected: boolean = false;
+  public item2Selected: boolean = false;
 
+  // Getter pour afficher le bouton global si l'un des deux est coché
+  get showGlobalValidate(): boolean {
+    return this.item1Selected || this.item2Selected;
+  }
 }
